2f1e274a5e18cb669fbba103896786a1b47781d8
Vee Trairattanapa <happierv@gmail.com>
2015-01-23 01:14:59 -0800

    Book Min & Test Case

--- Hacker Cup 15 - Cooking the Books/book.py
+++ Hacker Cup 15 - Cooking the Books/book.py
@@ -5,13 +5,19 @@ class Book:
 		self.length = len(self.input_number) # e.g. 5
 		self.digit_array = self.convert_to_digit_array() # e.g. [1, 2, 3, 4, 5]
 		self.cooked_max_number = self.cook_max() # e.g. 52341
-		print self.cooked_max_number
+		self.cooked_min_number = self.cook_min() # e.g. 12345
+		print self.cooked_min_number, self.cooked_max_number
 
 	def convert_to_digit_array(self):
 		output = []
 		for i in range(0, self.length):
 			output.append(int(self.input_number[i]))
 		return output
+
+	def cook_max(self):
+		max_digit, max_digit_index = self.get_max_digit() # e.g. 5, 4
+		output_digit_array = self.swap_digit(max_digit, max_digit_index) # e.g. [5, 2, 3, 4, 1]
+		return self.merge(output_digit_array) # e.g. 52341
 
 	def get_max_digit(self):
 		maxx = -1
@@ -22,23 +28,33 @@ class Book:
 				maxx_index = i
 		return maxx, maxx_index
 
-	def cook_max(self):
-		max_digit, max_digit_index = self.get_max_digit() # e.g. 5, 4
-		output_digit_array = self.swap_max_digit(max_digit, max_digit_index) # e.g. [5, 2, 3, 4, 1]
-		return self.merge(output_digit_array) # e.g. 52341
+	def cook_min(self):
+		min_digit, min_digit_index = self.get_min_digit()
+		output_digit_array = self.swap_digit(min_digit, min_digit_index)
+		return self.merge(output_digit_array)
 
-	def swap_max_digit(self, max_digit, max_digit_index):
+	def swap_digit(self, digit, digit_index):
 		output = list(self.digit_array)
-		if(max_digit != self.digit_array[0]):
-			output[0], output[max_digit_index] = self.digit_array[max_digit_index], self.digit_array[0]
+		if(digit != self.digit_array[0]):
+			output[0], output[digit_index] = self.digit_array[digit_index], self.digit_array[0]
 		return output
+
+	def get_min_digit(self):
+		minn = self.digit_array[0]
+		minn_index = 0
+		for i, number in enumerate(self.digit_array):
+			if(number < minn and number != 0):
+				minn = number
+				minn_index = i
+		return minn, minn_index
 
 	def merge(self, digit_array):
 		return ''.join(str(x) for x in digit_array)
 
-
-n = input('number of test cases: ')
+n = input()
 for i in range(0, int(n)):
 	num = input()
 	print 'Case #'+str(i+1)+':',
 	Book(num)
+
+# python book.py < input.txt > output2.txt 
